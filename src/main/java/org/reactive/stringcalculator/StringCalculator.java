dc98068a7860a8fff2f7b3d0a7c89f57e22141bf
Rajkishore <mrajkishor331@gmail.com>
2024-10-11 16:18:41 +0530

    feat(case01):  initial implementation to return 0

---
+++ src/main/java/org/reactive/stringcalculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package org.reactive.stringcalculator;
+
+public class StringCalculator {
+        public int add(String numbers) {
+            if (numbers == null || numbers.isEmpty()) {
+                return 0;
+            }
+            return 0;
+        }
+}
