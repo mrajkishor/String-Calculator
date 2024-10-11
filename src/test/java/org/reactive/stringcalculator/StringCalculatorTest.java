1b61be485edfa4039cc4d792655be70eb4c03698
Rajkishore <mrajkishor331@gmail.com>
2024-10-11 16:21:49 +0530

    test(case02):Add test for single number input

--- src/test/java/org/reactive/stringcalculator/StringCalculatorTest.java
+++ src/test/java/org/reactive/stringcalculator/StringCalculatorTest.java
@@ -9,4 +9,11 @@ public class StringCalculatorTest {
         assertEquals(0, calculator.add(""));
     }
 
+    @Test
+    public void testSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+
+
 }
