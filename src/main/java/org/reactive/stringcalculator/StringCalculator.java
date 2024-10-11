15b1dfb4c2f3bfbb4d3d05e7a413d551acbbc2c1
Rajkishore <mrajkishor331@gmail.com>
2024-10-11 16:33:57 +0530

    feat(case05): Support for new line delimiters in the add method

--- src/main/java/org/reactive/stringcalculator/StringCalculator.java
+++ src/main/java/org/reactive/stringcalculator/StringCalculator.java
@@ -5,7 +5,7 @@ public class StringCalculator {
             if (numbers == null || numbers.isEmpty()) {
                 return 0;
             }
-            String[] numberArray = numbers.split(",");
+            String[] numberArray = numbers.split(",|\n");
             int sum = 0;
             for (String num : numberArray) {
                 sum += Integer.parseInt(num);
